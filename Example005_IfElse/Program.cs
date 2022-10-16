49c6bff10e6ebdcb4c53854a306269a7e03eb926
Heupoxupypr <vasyakus@gmail.com>
2022-10-16 21:14:17 +0300

    Add Example005 If Else

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter your USERNAME:");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")  //ToLower() - переводит значение переменной в нижний регистр
+{
+    Console.WriteLine("URA! This is MASHA!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
